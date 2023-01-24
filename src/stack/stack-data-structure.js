b448c9914d3f20b61199fb46ce359ce3e770781a
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-01-24 18:19:27 +0100

    lab solved

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,39 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else return false;
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true; 
+    } else return false;
   }
 
   push(item) {
     // ... your code goes here
+    let checkTrue = this.canPush()
+    if (checkTrue == true) {
+      this.stackControl.push(item);
+      return this.stackControl;
+    } else throw new Error('STACK_OVERFLOW');
   }
 
   pop() {
     // ... your code goes here
+    let checkTrue = this.isEmpty ();
+    if (checkTrue == false) {
+      let deletedItem = this.stackControl.pop();
+      return deletedItem;
+    } else throw new Error('STACK_UNDERFLOW');
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl;
   }  
 }
 
